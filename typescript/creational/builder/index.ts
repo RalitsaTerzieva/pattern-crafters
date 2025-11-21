90348c533d21de430c9d4e777128f7643d71ca40
Ralica Popova <ralicaterzieva90@gmail.com>
2025-11-21 16:50:53 +0200

    Add customerbuilder interface and class

--- typescript/creational/builder/index.ts
+++ typescript/creational/builder/index.ts
@@ -5,6 +5,14 @@ interface ICustomer {
     phoneNumber: string;
 }
 
+interface ICustomerBuilder {
+    setFirstName(firstName: string): ICustomerBuilder;
+    setLastName(lastName: string): ICustomerBuilder;
+    setEmail(email: string): ICustomerBuilder;
+    setPhoneNumber(phoneNumber: string): ICustomerBuilder;
+    //build(): ICustomer;
+}
+
 class Customer implements ICustomer {
     constructor(
         public firstName: string,
@@ -12,4 +20,31 @@ class Customer implements ICustomer {
         public email: string,
         public phoneNumber: string
     ) {}
+}
+
+class CustomerBuilder implements ICustomerBuilder {
+    private firstName: string = "";
+    private lastName: string = "";
+    private email: string = "";
+    private phoneNumber: string = "";
+
+    setFirstName(firstName: string): ICustomerBuilder {
+        this.firstName = firstName;
+        return this
+    }
+
+    setLastName(lastName: string): ICustomerBuilder {
+        this.lastName = lastName;
+        return this
+    }
+
+    setEmail(email: string): ICustomerBuilder {
+        this.email = email;
+        return this
+    }
+
+    setPhoneNumber(phoneNumber: string): ICustomerBuilder {
+        this.phoneNumber = phoneNumber;
+        return this
+    }
 }
