da511af16f8edf06df67d63be36f897ed0183208
Ralica Popova <ralicaterzieva90@gmail.com>
2025-12-01 12:37:18 +0200

    Add two additional classes

--- typescript/structural/bridge/index.ts
+++ typescript/structural/bridge/index.ts
@@ -2,3 +2,23 @@ interface MediaPlayerImplementation {
     playAudio(): void;
     playVideo(): void;
 }
+
+class WindowsmediaPlayer implements MediaPlayerImplementation {
+    public playAudio() {
+        console.log("Playing audio on windows media player!")
+    }
+
+    public playVideo() {
+        console.log("Playing video on windows media player!")
+    }
+}
+
+class MacosmediaPlayer implements MediaPlayerImplementation {
+    public playAudio() {
+        console.log("Playing audio on macos media player!")
+    }
+
+    public playVideo() {
+        console.log("Playing video on macos media player!")
+    }
+}
