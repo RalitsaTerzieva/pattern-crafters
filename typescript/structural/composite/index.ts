e9f3df063aa822dc19a0be2007423193a5ac7ae2
Ralica Popova <ralicaterzieva90@gmail.com>
2025-12-04 15:15:00 +0200

    Add Manager class

--- typescript/structural/composite/index.ts
+++ typescript/structural/composite/index.ts
@@ -4,7 +4,7 @@ interface Employee {
     getRole(): string;
 }
 
-interface CompositeEmployee {
+interface CompositeEmployee extends Employee{
     addEmployee(employee: Employee): void;
     removeEmployee(employee: Employee): void;
     getEmployee(): Employee[];
@@ -45,5 +45,43 @@ class Designer implements Employee{
 
     public getRole() {
         return "Designer"
+    }
+}
+
+class Manager implements CompositeEmployee {
+    private employees: Employee[] = [];
+
+    constructor(
+        private name: string,
+        private salary: number
+    ) {}
+
+    public getName() {
+        return this.name
+    }
+
+    public getSalary() {
+        return this.salary
+    }
+
+    public getRole() {
+        return "Manager"
+    }
+
+    public addEmployee(employee: Employee): void {
+        this.employees.push(employee);
+    }
+
+    public removeEmployee(employee: Employee): void {
+        const index = this.employees.indexOf(employee);
+
+        if(index !== -1) {
+            this.employees.splice(index, 1);
+        }
+        
+    }
+
+    public getEmployee(): Employee[] {
+        return this.employees
     }
 }
