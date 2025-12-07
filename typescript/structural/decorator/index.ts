7e89cfeaa2c9d57872b7b0446f13e52075187c84
Ralica Popova <ralicaterzieva90@gmail.com>
2025-12-07 12:42:13 +0200

    Add concrete component class  logic

--- typescript/structural/decorator/index.ts
+++ typescript/structural/decorator/index.ts
@@ -2,3 +2,13 @@ interface Coffee {
     cost(): number;
     description(): string;
 }
+
+class SimpleCoffee implements Coffee {
+    public cost(): number {
+        return 10;
+    }
+
+    public description(): string {
+        return "This is simple coffee."
+    }
+}
