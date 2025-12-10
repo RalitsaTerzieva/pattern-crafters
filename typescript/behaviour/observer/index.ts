e58670737cf244201502ccecf6eeb5e072152212
Ralica Popova <ralicaterzieva90@gmail.com>
2025-12-10 14:00:29 +0200

    Test observer pattern

--- typescript/behaviour/observer/index.ts
+++ typescript/behaviour/observer/index.ts
@@ -57,3 +57,13 @@ class ConcreteSubject implements Subject {
         this.notifyObservers();
     }
 }
+
+const subject = new ConcreteSubject();
+
+const observer1 = new ConcreteObserver(1);
+subject.addObserver(observer1);
+
+const observer2 = new ConcreteObserver(2);
+subject.addObserver(observer2);
+
+subject.setState(123);
