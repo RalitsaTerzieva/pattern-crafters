da4c1a6cebb7a9c46209f47d82fdb13536f9b90a
Ralica Popova <ralicaterzieva90@gmail.com>
2025-12-12 16:48:33 +0200

    Implement the pattern classes

--- typescript/behaviour/strategy/index.ts
+++ typescript/behaviour/strategy/index.ts
@@ -1,3 +1,43 @@
+//called strategy
 interface PaymentStrategy {
     pay(amount: number): void;
+}
+   
+//called concrete strategy
+class PayPalStrategy implements PaymentStrategy {
+    public pay(amount: number): void {
+        console.log(`Paid $${amount} using PayPal.`)
+    }
+}
+
+class CreditCardStrategy implements PaymentStrategy {
+    public pay(amount: number): void {
+        console.log(`Paid $${amount} using Credit Card.`)
+    }
+}
+
+class BitcoinStrategy implements PaymentStrategy {
+    public pay(amount: number): void {
+        console.log(`Paid $${amount} using Bitcoin.`)
+    }
+}
+
+//called context
+class ShoppingCart {
+    private amount: number = 0;
+
+    constructor(private strategy: PaymentStrategy) {}
+
+    public setPaymentStrategy(strategy: PaymentStrategy): void {
+        this.strategy = strategy;
+    }
+
+    public addToCart(value: number): void {
+        this.amount += value;
+    }
+
+    public checkout(): void {
+        this.strategy.pay(this.amount);
+        this.amount = 0;
+    }
 }
