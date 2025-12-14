2df6cb1fcbed06085ae7bf66a5bb25d201a15716
Ralica Popova <ralicaterzieva90@gmail.com>
2025-12-14 17:13:52 +0200

    Add concrete classes and receiver class

--- typescript/behaviour/command/index.ts
+++ typescript/behaviour/command/index.ts
@@ -2,3 +2,37 @@ interface ICommand {
     execute(): void;
     undo(): void;
 }
+
+class Light {
+    public turnOn():void {
+        console.log("The light is on");
+    }
+
+    public turnOff(): void {
+        console.log("The light is off");
+    }
+}
+
+class TurnOnCommand implements ICommand {
+    constructor(private light: Light) {}
+    
+    public execute(): void {
+        this.light.turnOn();
+    }
+
+    public undo(): void {
+        this.light.turnOff();
+    }
+}
+
+class TurnOffCommand implements ICommand {
+    constructor(private light: Light) {}
+    
+    public execute(): void {
+        this.light.turnOff();
+    }
+
+    public undo(): void {
+        this.light.turnOn();
+    }
+}
